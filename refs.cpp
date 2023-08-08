1973c901bb9236036fb95d241ec6ff4c5942a981
Cameron-Ord <cameron.ian.ronald.ord@Outlook.com>
2023-08-08 16:26:52 -0600

    ref memory address

--- refs.cpp
+++ refs.cpp
@@ -12,4 +12,6 @@ int main()
     cout << meal << "\n";  // Outputs Pizza
 
     cout << &food << "\n"; //outputs memory address
+
+    // refs and pointers are important in C++, because they give you the ability to manipulate the data in the computer's memory - which can reduce the code and improve the performance. //
 }
