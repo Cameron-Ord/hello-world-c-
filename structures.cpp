5343a16d51301dcf08dfe1d4c04737411f94a426
Cameron-Ord <cameron.ian.ronald.ord@Outlook.com>
2023-08-08 16:21:31 -0600

    structures

--- structures.cpp
+++ structures.cpp
@@ -3,6 +3,12 @@
 #include <string>
 using namespace std;
 
+struct car {
+  string brand;
+  string model;
+  int year;
+};
+
 int main()
 {
     struct {             // Structure declaration
@@ -17,4 +23,25 @@ int main()
     // Print members of myStructure
     cout << myStructure.myNum << "\n";
     cout << myStructure.myString << "\n";
+
+    // Create a car structure and store it in myCar1;
+    car myCar1;
+    myCar1.brand = "BMW";
+    myCar1.model = "X5";
+    myCar1.year = 1999;
+
+    // Create another car structure and store it in myCar2;
+    car myCar2;
+    myCar2.brand = "Ford";
+    myCar2.model = "Mustang";
+    myCar2.year = 1969;
+    
+    // Print the structure members
+    cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+    cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+    
+    return 0;
+
+
+    
 }
